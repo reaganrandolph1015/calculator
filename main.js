911e279d52189ce68e8567482e66ef1b80271aaf
Reagan <reaganrandolph1015@gmail.com>
2022-01-17 15:53:32 -0600

    Added operator functions and operate()

---
+++ main.js
@@ -0,0 +1,20 @@
+// operator functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a / b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
